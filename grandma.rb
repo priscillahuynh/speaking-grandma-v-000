4cb6a918a017e056b58234c5f24311c74f9b0567
Priscilla H <priscillamhuynh@gmail.com>
2018-06-19 10:19:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif (phrase==phrase.upcase) == false
     return 'HUH?! SPEAK UP, SONNY!'
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
