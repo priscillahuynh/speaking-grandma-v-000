d54af1f86c136c5672bb907988572fe81069e500
Priscilla H <priscillamhuynh@gmail.com>
2018-06-19 10:09:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase=='I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase=='Hi Nana,how are you?'.downcase
+  elsif phrase=='Hi Nana,how are you?.downcase'
     return 'HUH?! SPEAK UP, SONNY!'
   end
 end
