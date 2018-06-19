9cf52d67a0cd8fbec3671ebcedac100157fac796
Priscilla H <priscillamhuynh@gmail.com>
2018-06-19 10:02:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase="I LOVE YOU GRANDMA!")
+def speak_to_grandma(phrase)
   if phrase=='I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN'
   end
